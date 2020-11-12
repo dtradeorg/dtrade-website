80c3b9a61632717f601765af9155b061db5cb640
kracodes2325 <asadanwer1@outlook.com>
2020-11-12 21:57:57 +0500

    beta launch v1

--- src/Component/navbar/Nav.js
+++ src/Component/navbar/Nav.js
@@ -15,15 +15,25 @@ class NavBar extends Component {
                             </h3>                
                 </Nav>      
                 <Nav>
-                    <a href="http://stakr.dtrade.org/" target="_blank" rel="noopener noreferrer" className="ml-4">
+                    <a href="http://stakr.dtrade.org/" target="_blank" rel="noopener noreferrer" className="ml-4 link">
                             Launch App
                             <img className="ml-arrow mb-1" src="./assets/images/arrow-top-right.svg" alt="arrow"></img>
                         </a>
-                    <img src="./assets/images/Navbar/dUSD.svg" alt="logo" className="img-fluid"/>
-                    <img src="./assets/images/Navbar/dUSD-1.svg" alt="logo" className="img-fluid social-link-margin"/>
-                    <img src="./assets/images/Navbar/dUSD-2.svg" alt="logo" className="img-fluid social-link-margin"/>
-                    <img src="./assets/images/Navbar/dUSD-3.svg" alt="logo" className="img-fluid social-link-margin"/>
-                    <img src="./assets/images/Navbar/dUSD-4.svg" alt="logo" className="img-fluid social-link-margin"/>
+                    <a href="https://medium.com/dtrade"  target="_blank" rel="noopener noreferrer" >  
+                    <img src="./assets/images/Navbar/dUSD.svg" alt="logo" className="img-fluid social-link-margin"/>
+                    </a>  
+                    <a href="https://twitter.com/dtradeorg"  target="_blank" rel="noopener noreferrer">
+                    <img  src="./assets/images/Navbar/dUSD-1.svg" alt="logo" className="img-fluid social-link-margin"/>
+                    </a>
+                    <a href="https://github.com/dtradeorg"  target="_blank" rel="noopener noreferrer">
+                    <img  src="./assets/images/Navbar/dUSD-2.svg" alt="logo" className="img-fluid social-link-margin"/>
+                    </a>
+                    <a href="https://discord.gg/hyS8E7BPT6"  target="_blank" rel="noopener noreferrer">
+                    <img  src="./assets/images/Navbar/dUSD-3.svg" alt="logo" className="img-fluid social-link-margin"/>
+                    </a>
+                    <a href="https://commonwealth.im/dtrade"  target="_blank" rel="noopener noreferrer">
+                    <img  src="./assets/images/Navbar/dUSD-4.svg" alt="logo" className="img-fluid social-link-margin"/>
+                    </a>
                 </Nav>        
 
             </Navbar>
