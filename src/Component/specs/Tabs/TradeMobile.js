0f6396baf973f2e28a72455c65f1949d54b06c0e
Amman Pasha <amman@seedcode.io>
2020-12-29 16:12:42 +0500

    Updated tradesection image

--- src/Component/specs/Tabs/TradeMobile.js
+++ src/Component/specs/Tabs/TradeMobile.js
@@ -1,27 +1,23 @@
-import React from 'react'
-import '../Specs.css'
-import { Animated } from 'react-animated-css'
+import React from 'react';
+import '../Specs.css';
+import { Animated } from 'react-animated-css';
 
 function TradeMobile() {
-    return (
-        <Animated animationIn="fadeIn" animationOut="fadeOut" > 
+  return (
+    <Animated animationIn='fadeIn' animationOut='fadeOut'>
+      <div className='row align-items-center justify-content-center'>
+        <div className='col-12 justify-content-center text-center tab-sec-mobile'>
+          <h1 className='reward-heading text-center'>Trade</h1>
+          <p className='reward-text text-center'>
+            Gain access to an array of financial markets, futures and margin trading with zero slippage. From anywhere
+            in the world.
+          </p>
 
-        <div className="row align-items-center justify-content-center">
-                            <div className="col-12 justify-content-center text-center tab-sec-mobile">
-                            <h1 className="reward-heading text-center">Trade</h1>
-                                <p className="reward-text text-center">
-                                Gain access to an array of financial markets, futures and margin trading with zero slippage.
-                                From anywhere in the world.
-                                </p>
-
-                                <img className='img-fluid tablet-trade' src="./assets/images/Specs/stake1.png" alt=""></img>
-
-
-                            </div>
-                        </div>
-                        </Animated>
-
-    )
+          <img className='img-fluid tablet-trade' src='./assets/images/Specs/trade.png' alt=''></img>
+        </div>
+      </div>
+    </Animated>
+  );
 }
 
-export default TradeMobile
+export default TradeMobile;
