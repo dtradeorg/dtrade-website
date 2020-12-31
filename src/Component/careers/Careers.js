9844e606231b163daa53324bc581d9a51c32f210
Tarun Kumar <ttarunkkumar99@gmail.com>
2020-12-31 21:32:53 +0500

    Add tabs for job categories

--- src/Component/careers/Careers.js
+++ src/Component/careers/Careers.js
@@ -1,149 +1,174 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import './careers.css';
-
+import React, { useState } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Nav,
+  NavItem,
+  NavLink,
+  TabContent,
+  TabPane,
+} from "react-bootstrap";
+import "./careers.css";
+const sample_job_data = {
+  Engineering: [
+    { jobTitle: "DevOps Engineer", location: "Worldwide", learnMore: "" },
+    { jobTitle: "Full Stack Developer", location: "Worldwide", learnMore: "" },
+    {
+      jobTitle: "Senior Backend Engineer",
+      location: "Worldwide",
+      learnMore: "",
+    },
+  ],
+  Test1: [
+    { jobTitle: "Test Job 1", location: "Worldwide", learnMore: "" },
+    { jobTitle: "Test Job 2", location: "Worldwide", learnMore: "" },
+  ],
+  Test2: [{ jobTitle: "Test Job 1", location: "Worldwide", learnMore: "" }],
+};
 function Careers() {
+  const [activeTab, setActiveTab] = useState("Engineering");
   return (
-    <>
-      <Container fluid className='bg'>
-        <Row className='padding'>
-          <Col className='text-left col-2'>
-            <a rel='nofollow' href='/'>
-              <img src='./assets/images/Navbar/Union.svg' alt='logo' className='img-fluid logo-career' />
+    <React.Fragment>
+      <Container fluid className="bg">
+        <Row className="padding">
+          <Col className="text-left col-2">
+            <a rel="nofollow" href="/">
+              <img
+                src="./assets/images/Navbar/Union.svg"
+                alt="logo"
+                className="img-fluid logo-career"
+              />
             </a>
           </Col>
-          <Col className='text-right p-0'>
-            <a href='https://medium.com/dtrade' target='_blank' rel='nofollow noopener noreferrer'>
-              <img src='./assets/images/Navbar/dUSD.svg' alt='logo' className='img-fluid social-link-margin-1' />
+          <Col className="text-right p-0">
+            <a
+              href="https://medium.com/dtrade"
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+            >
+              <img
+                src="./assets/images/Navbar/dUSD.svg"
+                alt="logo"
+                className="img-fluid social-link-margin-1"
+              />
             </a>
-            <a href='https://twitter.com/dtradeorg' target='_blank' rel='nofollow noopener noreferrer'>
-              <img src='./assets/images/Navbar/dUSD-1.svg' alt='logo' className='img-fluid social-link-margin-1' />
+            <a
+              href="https://twitter.com/dtradeorg"
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+            >
+              <img
+                src="./assets/images/Navbar/dUSD-1.svg"
+                alt="logo"
+                className="img-fluid social-link-margin-1"
+              />
             </a>
-            <a href='https://github.com/dtradeorg' target='_blank' rel='nofollow noopener noreferrer'>
-              <img src='./assets/images/Navbar/dUSD-2.svg' alt='logo' className='img-fluid social-link-margin-1' />
+            <a
+              href="https://github.com/dtradeorg"
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+            >
+              <img
+                src="./assets/images/Navbar/dUSD-2.svg"
+                alt="logo"
+                className="img-fluid social-link-margin-1"
+              />
             </a>
-            <a href='https://discord.gg/hyS8E7BPT6' target='_blank' rel='nofollow noopener noreferrer'>
-              <img src='./assets/images/Navbar/dUSD-3.svg' alt='logo' className='img-fluid social-link-margin-1' />
+            <a
+              href="https://discord.gg/hyS8E7BPT6"
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+            >
+              <img
+                src="./assets/images/Navbar/dUSD-3.svg"
+                alt="logo"
+                className="img-fluid social-link-margin-1"
+              />
             </a>
-            <a href='https://commonwealth.im/dtrade' target='_blank' rel='nofollow noopener noreferrer'>
-              <img src='./assets/images/Navbar/dUSD-4.svg' alt='logo' className='img-fluid social-link-margin-1' />
+            <a
+              href="https://commonwealth.im/dtrade"
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+            >
+              <img
+                src="./assets/images/Navbar/dUSD-4.svg"
+                alt="logo"
+                className="img-fluid social-link-margin-1"
+              />
             </a>
           </Col>
         </Row>
-
-        <Container className='title'>
-          <Row className='text-center'>
-            <Col md={12}>
-              <h3>Software Engineer</h3>
-            </Col>
-          </Row>
-          <Row className='text-center'>
-            <Col md={12}>
-              <h4>Remote</h4>
-            </Col>
-          </Row>
-        </Container>
-
-        <Container className='desc'>
-          <Row className='text-center'>
-            <Col md={12}>
-              <h3>
-                We're seeking a Backend Developer having proficiency in Node.js to join our team. We encourage people
-                who can dive headlong into situations to get things done, set their own direction and feel pride about
-                what they create. <br />
-                Your primary focus will be the development of all server-side logic, definition and maintenance of the
-                central database, and ensuring high performance and responsiveness to requests from the frontend. <br />{' '}
-                <br />
-                <a rel='nofollow' href='mailto:hi@dtrade.org'>
-                  Drop you Resume Here.
-                </a>
-              </h3>
-            </Col>
-          </Row>
-        </Container>
-
-        <Container className='list'>
-          <Row className='text-center'>
-            <Col md={12}>
-              <h3>Desired Skills & Experience</h3>
-            </Col>
-          </Row>
-          <ul className='p-0'>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              2+ years of professional backend experience
-            </li>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              Proficiency with JavaScript and in-depth knowledge of Node.js and its Express framework{' '}
-            </li>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              Understanding the nature of asynchronous programming and its quirks and workarounds{' '}
-            </li>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              Good understanding of server-side templating languages such as Jade and EJS{' '}
-            </li>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              Good understanding of server-side CSS preprocessors{' '}
-            </li>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              Basic understanding of front-end technologies, such as HTML5, and CSS3{' '}
-            </li>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              User authentication and authorization between multiple systems, servers, and environments{' '}
-            </li>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              Understanding fundamental design principles behind a scalable application{' '}
-            </li>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              Understanding differences between multiple delivery platforms, such as mobile vs. desktop, and
-              <span className='pl-4'> optimizing output to match the specific platform</span>{' '}
-            </li>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              Creating database schemas that represent and support business processes{' '}
-            </li>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              Proficient understanding of code versioning tools, such as Git{' '}
-            </li>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              Excellent oral and written communication will be a plus{' '}
-            </li>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              Must be a team player!{' '}
-            </li>
-          </ul>
-        </Container>
-
-        <Container className='list'>
-          <Row className='text-center'>
-            <Col md={12}>
-              <h3>Benefits & Perks</h3>
-            </Col>
-          </Row>
-          <ul className='p-0'>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              Competitive Base Salary + Stock Options + Equity
-            </li>
-            <li>
-              <img src='./assets/images/dot.png' alt='.' className='mr-3' />
-              Comprehensive healthcare benefits
-            </li>
-          </ul>
-        </Container>
       </Container>
-    </>
+      <div className="wrapper">
+        <section className="join-sec">
+          <Container>
+            <div className="row">
+              <Col sm={12}>
+                <div className="content-row">
+                  <h1 className="title">Join Us</h1>
+                  <p>
+                    We're seeking a Backend Developer having proficiency in
+                    Node.js to join our team. We encourage people who can dive
+                    headlong into situations to get things done, set their own
+                    direction and feel pride about what they create.
+                  </p>
+                  <p>
+                    Your primary focus will be the development of all
+                    server-side logic, definition and maintenance of the central
+                    database, and ensuring high performance and responsiveness
+                    to requests from the frontend.
+                  </p>
+                </div>
+                <div className="job-box-row">
+                  <h2 className="stitle">Available Positions</h2>
+                  <Nav className="openings-tab">
+                    {Object.entries(sample_job_data).map(([objKey, value]) => {
+                      return (
+                        value.length > 0 && (
+                          <NavItem>
+                            <NavLink
+                              active={activeTab === objKey ? true : false}
+                              onClick={() => {
+                                setActiveTab(objKey);
+                              }}
+                            >
+                              {objKey}
+                              <span>{value.length}</span>
+                            </NavLink>
+                          </NavItem>
+                        )
+                      );
+                    })}
+                  </Nav>
+                  <TabContent>
+                    <TabPane active={true}>
+                      <ul>
+                        {sample_job_data[activeTab]?.map((item) => {
+                          return (
+                            <li>
+                              <h6 className="st">{item.jobTitle}</h6>
+                              <span className="loc">{item.location}</span>
+                              <a
+                                className="clink"
+                                href={item.learnMore}
+                                title="Learn More"
+                              >
+                                Learn More
+                              </a>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </TabPane>
+                  </TabContent>
+                </div>
+              </Col>
+            </div>
+          </Container>
+        </section>
+      </div>
+    </React.Fragment>
   );
 }
 
